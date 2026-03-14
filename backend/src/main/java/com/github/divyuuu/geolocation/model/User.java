0a0e0fdbaae5da29ada1261819d0ba1d0cbed726
DivyanshuPant <divyanshu.pant117@gmail.com>
2026-03-15 01:31:55 +0530

    refactor(backend): enforce unique user_id in Driver and clean up User entity

--- backend/src/main/java/com/github/divyuuu/geolocation/model/User.java
+++ backend/src/main/java/com/github/divyuuu/geolocation/model/User.java
@@ -19,24 +19,16 @@ public class User {
     @GeneratedValue(strategy = GenerationType.UUID)
     @Setter(AccessLevel.NONE)
     private UUID uuid;
-
     private String name;
-
     @Column(unique = true, nullable = false)
     private String email;
-
     private String password;
-
     @Enumerated(EnumType.STRING)
     private Role role;
-
     private Double rating;
-
     private Integer totalRides;
-
     @OneToOne(mappedBy = "user")
     private Driver driver;
-
     @OneToOne(mappedBy = "user")
     private Passenger passenger;
 }
